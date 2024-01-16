63305d31b954c054c2714e54f08ce9e8cee6f30b
wertzui <wertzui@spielebeet.de>
2024-01-16 12:01:08 +0100

    @wertzui/ngx-restworld-client 13.1.0
    Fixed Delete function in list view
    Button to edit an item in a list view now opens the item in a new tab when the ctrl key is pressed

--- src/RESTworld/RESTworld.Client.Angular/projects/ngx-restworld-client/src/lib/components/restworld-menu-button/restworld-menu-button.component.ts
+++ src/RESTworld/RESTworld.Client.Angular/projects/ngx-restworld-client/src/lib/components/restworld-menu-button/restworld-menu-button.component.ts
@@ -16,25 +16,40 @@ export class RestWorldMenuButtonComponent {
    * An array of menu items to be displayed.
    */
   @Input({ required: true })
-  public items: MenuItem[]= [];
+  public items: MenuItem[] = [];
 
   constructor(private readonly _router: Router) {
 
   }
 
-  onClick(item: MenuItem) {
+  onClick(event: MouseEvent, item: MenuItem) {
+    const openInNewWindow = event.ctrlKey || event.button === 1 || event.metaKey;
     if (item.url) {
-      window.location.href = item.url;
-    } else if (item.routerLink) {
-      this._router.navigate(item.routerLink, {
-        queryParams: item.queryParams,
-        fragment: item.fragment,
-        queryParamsHandling: item.queryParamsHandling,
-        preserveFragment: item.preserveFragment,
-        skipLocationChange: item.skipLocationChange,
-        replaceUrl: item.replaceUrl
-      });
-    } else if (item.command) {
+      window.open(item.url, openInNewWindow ? '_blank' : '_self');
+    }
+    else if (item.routerLink) {
+      if (openInNewWindow) {
+        const tree = this._router.createUrlTree(item.routerLink, {
+          queryParams: item.queryParams,
+          fragment: item.fragment,
+          queryParamsHandling: item.queryParamsHandling,
+          preserveFragment: item.preserveFragment,
+        });
+        const url = this._router.serializeUrl(tree);
+        window.open(url, '_blank');
+      }
+      else {
+        this._router.navigate(item.routerLink, {
+          queryParams: item.queryParams,
+          fragment: item.fragment,
+          queryParamsHandling: item.queryParamsHandling,
+          preserveFragment: item.preserveFragment,
+          skipLocationChange: item.skipLocationChange,
+          replaceUrl: item.replaceUrl,
+        });
+      }
+    }
+    else if (item.command) {
       item.command({ item: item });
     }
   }
